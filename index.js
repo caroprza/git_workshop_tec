d9b4c31c1e8f11c85748113e1aa307c20958b3bc
Julian Orozco <orozco.castorena@outlook.com>
2019-04-22 11:09:47 -0500

    Add doc

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import { add, subtract, multiply, divide } from './Functions'
 
+// Output "Hello World"
 console.log('Hello World')
-console.log(add(1, 2))
+
+// Add numbers
+add(1, 2)
+// Substract numbers
+subtract(5, 4)
+// Multiply numbers
+multiply(3, 2)
+// Divide numbers
+divide(16, 4)
