12461bdf77f738e1f73823f38367cb19c6a59cce
Julian Orozco <orozco.castorena@outlook.com>
2019-04-22 21:49:12 -0500

    add prints

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import { add, subtract, multiply, divide } from './Functions'
 
-// Output "Hello World"
-console.log('Hello World')
-
 // Add numbers
-add(1, 2)
+console.log('Add', add(1, 2))
 // Substract numbers
-subtract(5, 4)
+console.log('Substract', subtract(5, 4))
 // Multiply numbers
-multiply(3, 2)
+console.log('Multiply', multiply(3, 2))
 // Divide numbers
-divide(16, 4)
+console.log('Divide', divide(16, 4))
